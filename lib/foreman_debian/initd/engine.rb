e752ded93516c83b9b3517dbdf66923600defc62
tomaszdurka <tomasz@durka.pl>
2015-06-23 13:48:39 +0200

    Use threads for engine.stop too

--- lib/foreman_debian/initd/engine.rb
+++ lib/foreman_debian/initd/engine.rb
@@ -32,27 +32,25 @@ module ForemanDebian
       def start
         threads = []
         each_file do |path|
-          threads << Thread.new { start_file(path) }
+          threads << Thread.new do
+            exec_command("#{path.to_s} start")
+            @output.info "  start  #{path.to_s}"
+            exec_command("update-rc.d #{path.basename} defaults") if path.dirname.eql? @system_export_path
+          end
         end
         ThreadsWait.all_waits(*threads)
       end
 
       def stop
+        threads = []
         each_file do |path|
-          stop_file(path)
+          @output.info "   stop  #{path.to_s}"
+          threads << Thread.new do
+            exec_command("#{path.to_s} stop")
+          end
+          exec_command("update-rc.d -f #{path.basename} remove") if path.dirname.eql? @system_export_path
         end
-      end
-
-      def start_file(path)
-        exec_command("#{path.to_s} start")
-        @output.info "  start  #{path.to_s}"
-        exec_command("update-rc.d #{path.basename} defaults") if path.dirname.eql? @system_export_path
-      end
-
-      def stop_file(path)
-        exec_command("#{path.to_s} stop")
-        @output.info "   stop  #{path.to_s}"
-        exec_command("update-rc.d -f #{path.basename} remove") if path.dirname.eql? @system_export_path
+        ThreadsWait.all_waits(*threads)
       end
 
       def remove_file(path)
